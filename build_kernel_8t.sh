98435cec351e13a34e7f8e9f5af1dd14ecffeb99
Coconut <coconutat@protonmail.com>
2026-03-21 14:29:55 +0800

    build script: change version

--- build_kernel_8t.sh
+++ build_kernel_8t.sh
@@ -48,9 +48,9 @@ if [ -f out/arch/arm64/boot/Image ]; then
 	cp out/arch/arm64/boot/Image tools/AnyKernel3/Image
 	cp -vf tools/AnyKernel3_sh/anykernel.sh tools/AnyKernel3/
 	cd tools/AnyKernel3
-	zip -r9 OnePlus-8T-LOS-23.0-${end_time}.zip * > /dev/null
+	zip -r9 OnePlus-8T-LOS-23.x-${end_time}.zip * > /dev/null
 	cd ../..
-	mv tools/AnyKernel3/OnePlus-8T-LOS-23.0-${end_time}.zip OnePlus-8T-LOS-23.0-${end_time}.zip
+	mv tools/AnyKernel3/OnePlus-8T-LOS-23.x-${end_time}.zip OnePlus-8T-LOS-23.x-${end_time}.zip
 	rm -rf tools/AnyKernel3/Image
 	echo "***Sucessfully built kernel...***"
 	echo " "
